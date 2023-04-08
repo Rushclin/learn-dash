40c0670c7f07ac6d8bba55ebb1227b655a78bf45
rushclin02 <takamrushclin@gmail.com>
2023-04-08 16:31:22 +0200

    Deploy

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-import os
-import pathlib
-
-from dash import dash, html, Output, Input, State, dcc
-import plotly.graph_objs as go
+from dash import dash, html, Output, Input, dcc
 import dash_bootstrap_components as dbc
 import dash_daq as daq
 
-import pandas as pd
 
 app = dash.Dash(
     __name__,
@@ -58,6 +53,104 @@ def render_header():
     )
 
 
+def render_tab_1():
+    return [
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.P(
+                            "Utilisez cette interface pour le parametrage du Dashboard"
+                        )
+                    ],
+                    id="",
+                    className="mt-5",
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.Label(
+                                    id="select",
+                                    children="Selectionnez la metrique",
+                                    className="mb-3",
+                                ),
+                                html.Br(),
+                                dcc.Dropdown(
+                                    options=[
+                                        "Facebook",
+                                        "WhatsApp",
+                                        "Telegram",
+                                    ],
+                                    id="select",
+                                ),
+                            ],
+                            md=4,
+                        ),
+                        dbc.Col(
+                            [
+                                html.Div(
+                                    generate_row(
+                                        "specification",
+                                        {
+                                            "borderBottom": "solid 2px #f1f1f1",
+                                            "height": "2rem",
+                                        },
+                                        {
+                                            "id": "m_header_1",
+                                            "children": html.Div("Specification"),
+                                        },
+                                        {
+                                            "id": "m_header_2",
+                                            "children": html.Div("Valeur"),
+                                        },
+                                        {
+                                            "id": "m_header_3",
+                                            "children": html.Div("Nouvelle valeur"),
+                                        },
+                                    ),
+                                ),
+                                html.Div(
+                                    [
+                                        generate_row(
+                                            "row_1",
+                                            {
+                                                "marginTop": "10px",
+                                                "marginBottom": "10px",
+                                            },
+                                            {
+                                                "id": "m_header_1",
+                                                "children": html.Div("Specification 1"),
+                                            },
+                                            {
+                                                "id": "m_header_1",
+                                                "children": html.Div("20"),
+                                            },
+                                            {
+                                                "id": "m_header_1",
+                                                "children": html.Div(
+                                                    dcc.Input(
+                                                        value=1,
+                                                        type="number",
+                                                        className="p-1",
+                                                    )
+                                                ),
+                                            },
+                                        )
+                                    ]
+                                ),
+                            ],
+                            md=8,
+                            className="px-5",
+                        ),
+                    ],
+                ),
+            ],
+            className="px-5",
+        )
+    ]
+
+
 def render_tabs():
     return html.Div(
         id="tabs",
@@ -88,7 +181,7 @@ def render_tabs():
     )
 
 
-def rendre_side_bar_graph():
+def render_side_bar_graph():
     return html.Div(
         id="",
         children=[
@@ -132,6 +225,112 @@ def rendre_side_bar_graph():
     )
 
 
+def render_header_list():
+    return generate_row(
+        "header_list",
+        {"height": "3rem", "margin": "1rem 0"},
+        {"id": "m_header_1", "children": html.Div("Parametres")},
+        {"id": "m_header_2", "children": html.Div("Total")},
+        {"id": "m_header_3", "children": html.Div("Evolution")},
+    )
+
+
+def generate_row(id, style, col1, col2, col3):
+    if style is None:
+        style = {"height": "8rem", "width": "100%"}
+    return dbc.Row(
+        children=[
+            dbc.Col(
+                id=col1["id"],
+                children=col1["children"],
+            ),
+            dbc.Col(
+                id=col2["id"],
+                children=col2["children"],
+            ),
+            dbc.Col(
+                id=col3["id"],
+                children=col3["children"],
+            ),
+        ],
+        id=id,
+        style=style,
+    )
+
+
+def render_content_top():
+    return html.Div(
+        children=[
+            dbc.Row(
+                children=[
+                    dbc.Col(
+                        [
+                            html.Div(
+                                ["Graphe de contrôle de metrique"],
+                                className="px-3 text-capitalize text-muted",
+                            ),
+                            html.Hr(),
+                            html.Div(
+                                id="",
+                                children=[
+                                    render_header_list(),
+                                    html.Div(
+                                        [
+                                            "Les elements ici",
+                                        ],
+                                        id="",
+                                    ),
+                                ],
+                                className="px-3",
+                            ),
+                        ],
+                        md=8,
+                        className="bg-dark p-2",
+                    ),
+                    dbc.Col(
+                        [
+                            html.Div(
+                                ["Les parametres"],
+                                className="px-3 text-capitalize text-muted",
+                            ),
+                            html.Hr(),
+                            dcc.Graph(
+                                id="",
+                                figure={
+                                    "data": [
+                                        {
+                                            "labels": [],
+                                            "values": [],
+                                            "type": "pie",
+                                            "marker": {
+                                                "line": {"color": "white", "width": 1}
+                                            },
+                                            "hoverinfo": "label",
+                                            "textinfo": "label",
+                                        }
+                                    ],
+                                    "layout": {
+                                        "margin": dict(l=20, r=20, t=20, b=20),
+                                        "showlegend": True,
+                                        "paper_bgcolor": "rgba(0,0,0,0)",
+                                        "plot_bgcolor": "rgba(0,0,0,0)",
+                                        "font": {"color": "white"},
+                                        "autosize": True,
+                                    },
+                                },
+                            ),
+                        ],
+                        md=4,
+                        className="bg-dark p-2",
+                    ),
+                ],
+                id="",
+            )
+        ],
+        className="px-3",
+    )
+
+
 app.layout = dbc.Container(
     fluid=True,
     children=[
@@ -152,19 +351,13 @@ app.layout = dbc.Container(
 )
 def render_tab_content(tab_switch):
     if tab_switch == "tab1":
-        return [
-            html.Div(
-                [
-                    html.H1("Element de la Tabs 1"),
-                ],
-            )
-        ]
+        return render_tab_1()
     return [
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        rendre_side_bar_graph(),
+                        render_side_bar_graph(),
                     ],
                     md=3,
                 ),
@@ -173,7 +366,7 @@ def render_tab_content(tab_switch):
                         html.Div(
                             id="graph_container",
                             children=[
-                                html.P("Les autres elements ici"),
+                                render_content_top(),
                             ],
                         ),
                     ],
