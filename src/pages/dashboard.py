904772c41c7e83578cdc44d6522e77ded3973d9a
rushclin02 <takamrushclin@gmail.com>
2023-04-12 11:56:28 +0200

    Edit dashboard

--- src/pages/dashboard.py
+++ src/pages/dashboard.py
@@ -1,17 +1,36 @@
 # DASHBOARD PAGE
 
 from dash import html
+from components.index import Component
+import dash_bootstrap_components as dbc
 
 
 class Dashboard:
-    def __init__(self):
+    def __init__(self, params, state_dic):
         print("============= INIT DASHBOARD PAGE ")
-        self.content = html.Div(
+        self.build_quick_stats_panel = Component().stat.build_quick_stats_panel()
+        self.build_top_panel = Component().graph.build_top_panel()
+
+        self.content = dbc.Row(
             [
-                html.H1(
-                    "Takam Rushclin",
+                dbc.Col(
+                    [
+                        self.build_quick_stats_panel,
+                    ],
+                    md=3,
+                ),
+                dbc.Col(
+                    [
+                        Component().graph.build_top_panel(),
+                    ],
+                    id="graphs-container",
+                    md=9,
+                    className="p-3"
+                    # children=[build_top_panel(stopped_interval), build_chart_panel()],
                 ),
             ],
+            id="status-container",
+            className="px-3",
         )
 
     def get_page(self):
