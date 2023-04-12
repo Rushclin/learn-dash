69fcd879da2a1aa8ddef908e4f452baa8c5f6282
rushclin02 <takamrushclin@gmail.com>
2023-04-12 11:55:49 +0200

    Edit settings

--- src/pages/settings.py
+++ src/pages/settings.py
@@ -1,16 +1,93 @@
 # DASHBOARD PAGE
 
-from dash import html
+from dash import html, dcc
+import dash_bootstrap_components as dbc
 
 
 class Settings:
-    def __init__(self):
+    def __init__(self, params):
         print("============= INIT SETTINGS PAGE 1 ")
 
+        self.params = params
         self.content = html.Div(
             [
-                html.H1(
-                    "Takam Rushclin setting",
+                html.Div(
+                    [
+                        html.P(
+                            "Utilisez les limites de contrôle historiques pour établir une référence ou définir de nouvelles valeurs.",
+                        )
+                    ],
+                    className="my-5 fw-bold",
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.Div(
+                                    [
+                                        html.Label(
+                                            "Selectionnez la metrique",
+                                            id="",
+                                            className="mb-3",
+                                        ),
+                                        html.Br(),
+                                        dcc.Dropdown(
+                                            id="metric-select-dropdown",
+                                            options=list(
+                                                {"label": param, "value": param}
+                                                for param in self.params[1:]
+                                            ),
+                                            value=self.params[1],
+                                        ),
+                                    ],
+                                    id="",
+                                ),
+                            ],
+                            md=4,
+                        ),
+                        dbc.Col(
+                            [
+                                html.Div(id="value-setter-panel"),
+                                html.Br(),
+                                html.Div(
+                                    id="value-setter-view-output",
+                                    className="output-datatable",
+                                ),
+                                html.Div(
+                                    [
+                                        dbc.Row(
+                                            [
+                                                dbc.Col(
+                                                    [
+                                                        html.Button(
+                                                            "Mettre à jour",
+                                                            id="value-setter-set-btn",
+                                                            className="btn btn-outline-primary",
+                                                        ),
+                                                    ],
+                                                    md=6,
+                                                ),
+                                                dbc.Col(
+                                                    [
+                                                        html.Button(
+                                                            "Etat courant",
+                                                            id="value-setter-view-btn",
+                                                            n_clicks=0,
+                                                            className="btn btn-outline-primary mr-5",
+                                                        ),
+                                                    ],
+                                                    md=6,
+                                                    className="align-right",
+                                                ),
+                                            ]
+                                        )
+                                    ],
+                                    className="mt-5",
+                                ),
+                            ],
+                            md=8,
+                        ),
+                    ]
                 ),
             ],
         )
