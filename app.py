62615342b4452da10f2de2f4508224339805fddc
Rushclin02 <takamrushclin@gmail.com>
2023-04-14 02:00:04 +0100

    Edit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from dash import dash 
+import d
 
 app = dash.Dash(title='My Board')
 
 print('Bonjour Takam')
 
-app.run_server(debug=True, port=8050)
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8050)
